f74e31e64e116467c3bffe21505f8065efe27159
Mrdeathc <54813946+Mrdeathc@users.noreply.github.com>
2019-09-12 12:36:17 -0500

    Mrdeathc

---
+++ js/boton.js
@@ -0,0 +1,19 @@
+//Get the button:
+mybutton = document.getElementById("fixedbutton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 210 || document.documentElement.scrollTop > 210) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+
+    if (document.body.scrollTop > 1814 || document.documentElement.scrollTop > 3200) {
+        mybutton.style.display = "none";
+    }
+}
